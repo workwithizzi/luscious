fb553507c2e2e9ac140a38a9771e8250c1b0e9a9
Yisrael Grimes <yisraelgrimes@gmail.com>
2018-10-19 18:17:07 -0500

    Prettier auto-formatting

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,14 @@
 // @since v0.0.6
 // ------------------------------------------------------------------
 
-
 // Plugins
 var g = require('gulp');
-		sass = require('gulp-sass'),
-		prefix = require('gulp-autoprefixer'),
-		sassLint = require('gulp-sass-lint'),
-		sourcemaps = require('gulp-sourcemaps'),
-		sassdoc = require('sassdoc'),
-		browserSync = require('browser-sync');
-
+(sass = require('gulp-sass')),
+	(prefix = require('gulp-autoprefixer')),
+	(sassLint = require('gulp-sass-lint')),
+	(sourcemaps = require('gulp-sourcemaps')),
+	(sassdoc = require('sassdoc')),
+	(browserSync = require('browser-sync'));
 
 // ------------------------------------
 // Paths
@@ -23,16 +21,15 @@ var styles = {
 		'./core/**/*.s+(a|c)ss',
 		// './scaffold/**/*.s+(a|c)ss'
 	],
-	dest: 'test/'
+	dest: 'test/',
 };
 
 // Sassdoc
 var docs = {
 	src: 'core/**/*.s+(a|c)ss',
 	dest: 'docs/',
-	watch: styles.dest + 'test.css'
-}
-
+	watch: styles.dest + 'test.css',
+};
 
 // ------------------------------------
 // Main Task
@@ -43,54 +40,55 @@ g.task('default', [
 	'docs',
 ]);
 
-
 // ------------------------------------
 // SASS
 // ------------------------------------
 g.task('styles', ['styles:build', 'styles:watch']);
 
-
 // - Compile Sass
 // - Create Sourcemaps
 // - Autoprefix
 g.task('styles:build', () => {
-	return g.src(styles.src)
+	return g
+		.src(styles.src)
 		.pipe(sourcemaps.init())
-		.pipe(sass({
-			errLogToConsole: true,
-			outputStyle: 'expanded',
-			sourceComments: 'true',
-			indentType: 'tab',
-			indentWidth: '1',
-			includePaths: [
-				'node_modules/susy/sass',
-				'node_modules/sass-true/sass',
-				'node_modules/modularscale-sass/stylesheets'
-			]
-		}).on('error', sass.logError))
+		.pipe(
+			sass({
+				errLogToConsole: true,
+				outputStyle: 'expanded',
+				sourceComments: 'true',
+				indentType: 'tab',
+				indentWidth: '1',
+				includePaths: [
+					'node_modules/susy/sass',
+					'node_modules/sass-true/sass',
+					'node_modules/modularscale-sass/stylesheets',
+				],
+			}).on('error', sass.logError)
+		)
 		.pipe(prefix({ browsers: ['last 2 versions'] }))
 		.pipe(sourcemaps.write({ includeContent: false }))
 		.pipe(g.dest(styles.dest))
 		.pipe(browserSync.reload({ stream: true }));
 });
 
-
 // Watch Sass
 g.task('styles:watch', () => {
 	g.watch(styles.src, ['styles']);
-})
-
+});
 
 // Lint Sass Files
 g.task('lint', () => {
-	return g.src(styles.src)
-		.pipe(sassLint({
-			configFile: './.sass-lint.yml'
-		}))
-		.pipe(sassLint.format())
-		// .pipe(sassLint.failOnError())
+	return g
+		.src(styles.src)
+		.pipe(
+			sassLint({
+				configFile: './.sass-lint.yml',
+			})
+		)
+		.pipe(sassLint.format());
+	// .pipe(sassLint.failOnError())
 });
-
 
 // ------------------------------------
 // Sassdoc
@@ -98,12 +96,7 @@ g.task('lint', () => {
 // - Watch & Compile Styles
 // - Watch & Compile Sassdoc
 // - Run local server
-g.task('docs', [
-	'styles:build',
-	'docs:build',
-	'docs:watch'
-]);
-
+g.task('docs', ['styles:build', 'docs:build', 'docs:watch']);
 
 // Use for dev and testing:
 // - Run Local Server
@@ -128,6 +121,5 @@ g.task('docs:watch', () => {
 
 // Use to produce static docs
 g.task('docs:build', () => {
-	return g.src(docs.src)
-		.pipe(sassdoc());
+	return g.src(docs.src).pipe(sassdoc());
 });
